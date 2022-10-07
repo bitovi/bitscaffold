5eec6a58b0c40e557d6496f8441060a6c0915810
Mark Repka <mrepka@bitovi.com>
2022-10-07 15:13:06 -0400

    SQL DDL

--- test/sequelize/schema.ts
+++ test/sequelize/schema.ts
@@ -3,6 +3,28 @@ export class Employee {
         return 'employee'
     }
 
+    static get fields() {
+        return {
+            id: {
+                type: "uuid",
+                primary: true,
+                required: true
+            },
+            name: {
+                type: "string",
+                required: true, // NOT NULL
+            },
+            start_date: {
+                type: "datetime",
+                required: false
+            },
+            end_date: {
+                type: "datetime",
+                required: false
+            }
+        }
+    }
+
     static get relationships() {
         return {
             skills: {
@@ -28,14 +50,16 @@ export class Skill {
         return 'skill'
     }
 
-    static get fields () {
+    static get fields() {
         return {
             id: {
                 type: "uuid",
-                primary: true
+                primary: true, // CONSTRAINT skill_pkey PRIMARY KEY (id)
+                required: true
             },
             name: {
                 type: "string",
+                unique: true, // CONSTRAINT skill_name_unique UNIQUE (name)
             }
         }
     }
@@ -57,3 +81,57 @@ export class Skill {
         }
     }
 }
+
+/**
+-- public.employee definition
+
+-- Drop table
+
+-- DROP TABLE employee;
+
+CREATE TABLE employee (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	"name" varchar(255) NOT NULL,
+	start_date date NULL,
+	end_date date NULL,
+	CONSTRAINT employee_pkey PRIMARY KEY (id)
+);
+ */
+
+
+/**
+-- public.skill definition
+
+-- Drop table
+
+-- DROP TABLE skill;
+
+CREATE TABLE skill (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	"name" varchar(255) NULL,
+	CONSTRAINT skill_name_unique UNIQUE (name),
+	CONSTRAINT skill_pkey PRIMARY KEY (id)
+);
+ */
+
+
+/**
+-- public.employee__skill definition
+
+-- Drop table
+
+-- DROP TABLE employee__skill;
+
+CREATE TABLE employee__skill (
+	employee_id uuid NOT NULL,
+	skill_id uuid NOT NULL,
+	CONSTRAINT employee__skill_pkey PRIMARY KEY (employee_id, skill_id)
+);
+
+
+-- public.employee__skill foreign keys
+
+ALTER TABLE public.employee__skill ADD CONSTRAINT employee__skill_employee_id_foreign FOREIGN KEY (employee_id) REFERENCES employee(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE public.employee__skill ADD CONSTRAINT employee__skill_skill_id_foreign FOREIGN KEY (skill_id) REFERENCES skill(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ */
+
