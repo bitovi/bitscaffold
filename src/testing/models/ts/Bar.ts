077704120166bd8785fdffed4c5af3bc8f78b30e
Mark Repka <mrepka@bitovi.com>
2022-11-17 16:10:14 -0500

    Cleanup old test model files

--- src/testing/models/ts/Bar.ts
+++
@@ -1,9 +0,0 @@
-import { ScaffoldModel, DataTypes } from "../../../types";
-
-export const Bar: ScaffoldModel = {
-  name: "Bar",
-  attributes: {
-    name: DataTypes.STRING,
-  },
-  belongsTo: [{ target: "Foo" }],
-};
